28862396d079ccf1e278c9aac19f0e1364b458cf
Andrew Andersen <77943779+pandamon99@users.noreply.github.com>
2021-03-07 03:38:24 -0500

    "demoable" status

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Auth from './Components/Auth/Auth'
 import {Home} from './Components/Home/Home'
 import Cart from './Components/Cart/Cart'
+import Browser from './Components/Browser/Browser'
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 import {Navigation} from './Components/Home/Navigation/Navigation'
 function App() {
@@ -18,7 +19,7 @@ function App() {
           </Route>
 
           <Route path = '/browse' exact = {true}>
-              
+              <Browser />
           </Route>
 
           <Route path = '/cart/:id?'>
