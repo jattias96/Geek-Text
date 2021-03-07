39eb14e7bca11645edf6ea9cac9a8673c915bdba
LeanetAlfonso <leanet.alfonso1@gmail.com>
2021-03-07 08:28:42 -0500

    made some fe changes

--- client/src/Components/Home/Navigation/Navigation.jsx
+++ client/src/Components/Home/Navigation/Navigation.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 import Logo from '../../../Assets/geek-text-logo.png'
 import './Navigation.css'
 import { Link } from 'react-router-dom'
@@ -14,9 +14,11 @@ export const Navigation = () => {
                 </Link>
             </div>
             <div className="nav-right">
+            <Link to="/" className="Router_Link">
                 <div className="nav-right-booklist nav-link">
                     <h4 className="links">View Books</h4>
                 </div>
+                </Link>
                 <div className="nav-right-auth nav-link">
                     <Link to="/auth" className="Router_Link">
                         <h4 className="links">Sign-up/Sign-in</h4>
@@ -27,7 +29,7 @@ export const Navigation = () => {
                 </div>
                 <div className="nav-right-cart nav-link">
                     <Link to="/cart" className="Router_Link">
-                        <ShoppingCartIcon fontSize="small" />
+                        <ShoppingCartOutlinedIcon fontSize="small" />
                     </Link>
                 </div>
             </div>
