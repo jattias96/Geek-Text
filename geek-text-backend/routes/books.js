f676cd48af955c72c21308bb6194db10f3a511e2
LeanetAlfonso <leanet.alfonso1@gmail.com>
2021-03-06 23:12:18 -0500

    added some frontend components and started functionality of shopping cart

--- geek-text-backend/routes/books.js
+++ geek-text-backend/routes/books.js
@@ -1,15 +1,16 @@
-import { Router } from 'express'
-import { Book } from '../models/bookModel.js'
+import expressAzyncHandler from 'express-async-handler';
+import { Router } from 'express';
+import { Book } from '../models/bookModel.js';
 const router = Router();
-// Handle get request
-router.route('/').get((req, res)=>{
-    Book.find()
+
+// Handle get request (all books)
+router.route('/').get(async(req, res)=>{
+    await Book.find()
     .then(books => res.json(books))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-
-// Handle post request
+// Handle post request (data population)
 router.route('/add').post((req,res) => {
     const newBook = new Book(req.body)
     newBook.save()
